fce066a854e605a947926dd37aaac0469b57446f
Najas06 <najasneju@gmail.com>
2025-06-21 22:06:02 +0530

    vercel analysis and speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "sonner";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const workSans = localFont({
   src: "./fonts/WorkSans-VariableFont_wght.ttf",
@@ -64,6 +66,8 @@ export default function RootLayout({
         {children}
         <Footer />
         <Toaster richColors position="top-center" />
+         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
